af1fbb7c9931c1c53905eadb9808194717442b41
julio gabriel <juliogabriel78956@gmail.com>
2025-12-03 14:59:36 -0300

    Primeiros passos

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
 import java.util.Scanner;
 
-public class Main{
-    public void main(String[] args){
+public class Main {
 
-        Scanner scanner;
-         System.out.println("Olá");
+    private final static String WELCOME_MESSAGE = "Olá, informe o seu nome";
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(WELCOME_MESSAGE);
+        var name = scanner.next();
+
+        System.out.println("Informe sua idade:");
+        var age = scanner.nextInt();
+
+        System.out.printf("Olá %s, sua idade é %d%n", name, age);
     }
-
 }
