d48dde872c7c67bc4368f95b1177cf64601ef6fd
julio gabriel <juliogabriel78956@gmail.com>
2025-12-11 19:14:47 -0300

    Estudos

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,15 @@
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        var scanner = new Scanner(System.in);
-
-        System.out.println("Informe seu nome:");
-        var name = scanner.next();
-
-        System.out.println("Informe sua idade:");
-        var age = scanner.nextInt();
-
-        if (age >= 17) {
-            System.out.printf("Olá %s! Você tem %d anos, você pode dirigir.\n", name, age);
-        }
-
-        System.out.println("Fim da execução");
+        var male = new Person();
+         male.setName("Gabriel");
+         male.setAge(13);
+         var female = new Person();
+         female.setName("Julio");
+         female.setAge(46);
+        System.out.println(" Male.name :" + male.getName() + " age " + male.getAge());
+        System.out.println(" Male.name :" + female.getName() + " age " + female.getAge());
     }
 }
 
---
+++ src/Person.java
@@ -0,0 +1,29 @@
+public class Person {
+
+
+    public static String teste() {
+        return "teste";
+    }
+
+    private String name;
+
+    private int age;
+
+    public String getName() {
+        return this.name;
+
+    }
+    public void setName( String name){
+        this.name = name;
+    }
+
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age){
+      this.age = age;
+    }
+
+}
